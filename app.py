5586ff20f96076bd9495e6734511b0bec4d67727
Prameya Sasmal <sasmalprameya@gmail.com>
2026-06-27 16:48:58 +0530

    Added certificate generation page

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/generate")
+def generate():
+    return render_template("generate.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
-    
